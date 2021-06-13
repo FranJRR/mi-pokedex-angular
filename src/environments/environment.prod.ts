3b98ee9fcec0bbbdeb0f9c46352b50ac86ea9f35
FranJRR <franciscojoserebollorendon@gmail.com>
2021-06-13 20:41:16 +0200

    envoroments firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+const firebaseConfig = {
+  apiKey: 'AIzaSyCGQCc2PCFEMU4PRBjzEUDyPPF7zDMzHns',
+  authDomain: 'pokeangular-d7a8b.firebaseapp.com',
+  databaseURL: 'https://pokeangular-d7a8b-default-rtdb.firebaseio.com',
+  projectId: 'pokeangular-d7a8b',
+  storageBucket: 'pokeangular-d7a8b.appspot.com',
+  messagingSenderId: '232961371596',
+  appId: '1:232961371596:web:22d21e49c7bee976bb0169',
+  measurementId: 'G-EY18XRL4C4'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+const firebaseConfig = {
+  apiKey: 'AIzaSyCGQCc2PCFEMU4PRBjzEUDyPPF7zDMzHns',
+  authDomain: 'pokeangular-d7a8b.firebaseapp.com',
+  databaseURL: 'https://pokeangular-d7a8b-default-rtdb.firebaseio.com',
+  projectId: 'pokeangular-d7a8b',
+  storageBucket: 'pokeangular-d7a8b.appspot.com',
+  messagingSenderId: '232961371596',
+  appId: '1:232961371596:web:22d21e49c7bee976bb0169',
+  measurementId: 'G-EY18XRL4C4'
+};
