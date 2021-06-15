306899fca15ea5cd194af4eca1cecacfdd21227e
FranJRR <franciscojoserebollorendon@gmail.com>
2021-06-15 19:25:20 +0200

    add login in route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 // Components
 import { InitialComponent } from './components/initial/initial.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
+// login
+import {Login} from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: InitialComponent, pathMatch: 'full' },
   { path: 'pokemon/:id', component: PokemonComponent },
+  { path: 'login', component: ogin },
+
   
 ];
 
